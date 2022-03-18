d05349bf7b19cd1f05becef148ced547db940a33
kreikrei <kreitonstrs@gmail.com>
2022-03-18 17:39:02 +0700

    integrate Nexus

--- src/DispatchOps.jl
+++ src/DispatchOps.jl
@@ -1,5 +1,18 @@
 module DispatchOps
 
-# Write your package code here.
+export
+    # simplenexus
+    Graph, Digraph, Arc, ArcIter, 
+    add_arc!, rem_arc!, add_node!, rem_node!, 
+    nn, na, nodes, arcs, fadj, badj, has_arc, has_node, 
+    src, tgt, key,
+
+    # metanexus
+    MetaGraph, MetaDigraph, props, get_prop, has_prop, 
+    set_props!, set_prop!, rem_prop!, clear_props!,
+    filter_arcs, filter_nodes
+
+include("Nexus/Nexus.jl")
+using .Nexus
 
 end
---
+++ src/Nexus/Nexus.jl
@@ -0,0 +1,32 @@
+module Nexus
+
+using SimpleTraits
+using Lazy
+
+import DispatchOps
+import Base:
+    show, eltype, Tuple, iterate, length, reverse, 
+    getindex, rand
+
+include("interface.jl")
+
+include("./simplenexus/arc.jl")
+include("./simplenexus/digraph.jl")
+include("./simplenexus/graph.jl")
+include("./simplenexus/arciter.jl")
+
+include("./metanexus/extension.jl")
+include("./metanexus/metadigraph.jl")
+include("./metanexus/metagraph.jl")
+
+export
+    Graph, Digraph, Arc, ArcIter, 
+    add_arc!, rem_arc!, add_node!, rem_node!, 
+    nn, na, nodes, arcs, fadj, badj, has_arc, has_node, 
+    src, tgt, key,
+
+    MetaGraph, MetaDigraph, props, get_prop, has_prop, 
+    set_props!, set_prop!, rem_prop!, clear_props!,
+    filter_arcs, filter_nodes
+
+end
