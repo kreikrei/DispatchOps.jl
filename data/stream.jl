838151a63216c70aa620683518436c594af19e8c
kreikrei <kreitonstrs@gmail.com>
2022-03-25 15:15:50 +0700

    data folder modifications

--- data/stream.jl
+++
@@ -1,8 +0,0 @@
-using CSV, DataFrames
-khazanah = CSV.read("khazanah.csv", DataFrame);
-demand = CSV.read("demand.csv", DataFrame);
-stock = CSV.read("stock.csv", DataFrame);
-periode = CSV.read("periode.csv", DataFrame);
-pecahan = CSV.read("pecahan.csv", DataFrame);
-trayek = CSV.read("trayek.csv", DataFrame);
-moda = CSV.read("moda.csv", DataFrame);
