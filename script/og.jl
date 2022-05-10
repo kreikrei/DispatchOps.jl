4f83628a0fc83db45f37da0ff961f1c4494944e1
kreikrei <kreitonstrs@gmail.com>
2022-05-10 09:33:18 +0700

    new run

--- script/og.jl
+++ script/og.jl
@@ -1,25 +1,30 @@
 using DispatchOps
 using JLD2
+using DataFrames
 
 const model_used = hard_holdover_model
 const T = 12
-const H_to_test = 1:3
+const H_to_test = 1:5
+const noise_to_test = 0:10:100
 
 # libs complete = true
-# sim fixed = false
+# sim fixed = true
 
-l = Libraries("/home/kreiton/.julia/dev/DispatchOps/data/og", complete=true)
-l.demand_realization = noisify_fixed(l.demand_forecast, 0)
+s = DataFrame(
+    H=Int[],
+    noise=Int[],
+    simulation=Simulation[]
+)
 
-s = Vector{Simulation}()
-for h in H_to_test
-    new_s = Simulation(
-        libs=l, params=Params(H=h, T=T, model=model_used), fixed=false
-    )
-    push!(s, new_s)
+for h in H_to_test, noise in noise_to_test
+    l = Libraries("/home/kreiton/.julia/dev/DispatchOps/data/og", complete=true)
+    l.demand_realization = noisify_fixed(l.demand_forecast, noise)
+    new_sim = Simulation(libs=l, params=Params(H=h, T=T, model=model_used), fixed=true)
+    append!(s, DataFrame(H=h, noise=noise, simulation=new_sim))
 end
 
-initiate!.(s)
-run!.(s)
+
+initiate!.(s.simulation)
+run!.(s.simulation)
 
 save_object("og.jld2", s)
