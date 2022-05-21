7eee7c1a5f87b95a3024d5155dae166489261cd3
kreikrei <kreitonstrs@gmail.com>
2022-05-21 13:25:59 +0700

    add todos for verification

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "DispatchOps.jl" begin
     # Write your tests here.
+    # TODO #53 test case for verification -> M - 1 + front loading
+    # TODO #54 test case for verification -> 1 - M + front loading
+    # TODO #55 test case for verification -> 1 - 1 + front loading
+    # TODO #56 test case for verification -> 0
 end
