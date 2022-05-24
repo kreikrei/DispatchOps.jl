224dd097485f4df765f2d440ded2802ad2470d61
kreikrei <kreitonstrs@gmail.com>
2022-05-24 17:05:41 +0700

    test case for verification -> 1 - 1 + front loading Fixes #55

--- test/runtests.jl
+++ test/runtests.jl
@@ -71,6 +71,7 @@ using Test
         initiate!(s)
         run!(s)
 
+        answer = MetaDigraph{DispatchOps.Sim.locper}()
         add_arc!(answer, DispatchOps.Sim.locper("B", 0), DispatchOps.Sim.locper("A", 1))
         add_arc!(answer, DispatchOps.Sim.locper("C", 0), DispatchOps.Sim.locper("A", 1))
         add_arc!(answer, DispatchOps.Sim.locper("D", 0), DispatchOps.Sim.locper("A", 1))
@@ -87,6 +88,36 @@ using Test
             end
         end
     end
-end
 
-# TODO #55 test case for verification -> 1 - 1 + front loading
+    @testset "nearest" begin
+        data_path = "/home/kreiton/.julia/dev/DispatchOps/data/dummy/nearest/"
+        T = 1
+        H = 1
+        is_complete = true
+        dist = Euclidean()
+        model = standard_model
+
+        answer = MetaDigraph{DispatchOps.Sim.locper}()
+        l = Libraries(data_path, complete=is_complete)
+        p = Params(H=H, T=T, model=model, dist=dist)
+        append!(l.demand_realization, noisify_fixed(l.demand_forecast, 0))
+
+        s = Simulation(libs=l, params=p)
+        initiate!(s)
+        run!(s)
+
+        add_arc!(answer, DispatchOps.Sim.locper("B", 0), DispatchOps.Sim.locper("C", 1))
+        add_arc!(answer, DispatchOps.Sim.locper("D", 0), DispatchOps.Sim.locper("E", 1))
+        for a in arcs(answer)
+            set_prop!(answer, a, :flow, Dict("NN" => 250))
+            set_prop!(answer, a, :trip, 1)
+        end
+        @test collect(arcs(s.acc.executed_dispatch)) == collect(arcs(answer))
+        if collect(arcs(s.acc.executed_dispatch)) == collect(arcs(answer))
+            for a in arcs(answer)
+                @test answer[a][:flow] == s.acc.executed_dispatch[a][:flow]
+                @test answer[a][:trip] == s.acc.executed_dispatch[a][:trip]
+            end
+        end
+    end
+end
