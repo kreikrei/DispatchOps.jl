97ca47d57db5f9e6e74d4c631be46330099c189e
kreikrei <kreitonstrs@gmail.com>
2022-05-24 16:41:38 +0700

    remove todos

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,6 @@ using DispatchOps
 using Test
 
 @testset "DispatchOps.jl" begin
-    # Write your tests here.
-    # TODO #53 test case for verification -> M - 1 + front loading
-    # TODO #54 test case for verification -> 1 - M + front loading
+
     # TODO #55 test case for verification -> 1 - 1 + front loading
-    # TODO #56 test case for verification -> 0
 end
