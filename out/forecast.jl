958eb8276c974ed9758099a50590095117ec8124
kreikrei <kreitonstrs@gmail.com>
2022-06-01 13:25:42 +0700

    forecast pake total_cost

--- out/forecast.jl
+++ out/forecast.jl
@@ -9,13 +9,6 @@ using Colors
 # using CairoMakie
 import Makie: Makie.wong_colors
 
-peti_transported(s::Simulation) = sum(
-    sum(
-        s.acc.executed_dispatch[a][:flow][k]
-        for (k, v) in s.acc.executed_dispatch[a][:flow]
-    ) for a in arcs(s.acc.executed_dispatch)
-)
-
 exp010 = load_object("/home/kreiton/.julia/dev/DispatchOps/out/exp010.jld2")
 exp011 = load_object("/home/kreiton/.julia/dev/DispatchOps/out/exp011.jld2")
 
@@ -27,25 +20,12 @@ transform!(
     expo, :simulation => ByRow(x -> total_cost(x) * 1e3) => :total_cost
 )
 
-pecahan = CSV.read("data/.pecahan.csv", DataFrame)
-pecahand = Dict(
-    pecahan.id .=> [
-        (nilai=p.nilai, konversi=p.konversi) for p in eachrow(pecahan)
-    ]
-)
-
-transform!(expo,
-    :simulation => ByRow(x -> sum(abs.([r.value * pecahand[r.pecahan].konversi * pecahand[r.pecahan].nilai for r in eachrow(x.acc.demand_fulfillment)]))) => :demand_fulfilled
-)
-
-insertcols!(expo, :fulfillment_cost => expo.demand_fulfilled ./ expo.total_cost)
-
 transform!(
-    exp, :simulation => ByRow(x -> lost_sales(x)) => :lost_sales
+    expo, :simulation => ByRow(x -> lost_sales(x)) => :lost_sales
 )
 
-p_noise_fulfillment = plot(expo,
-    x=:noise, y=:fulfillment_cost, xgroup=:noise_function, color=:H,
+p_noise_total_cost = plot(expo,
+    x=:noise, y=:total_cost, xgroup=:noise_function, color=:H,
     Geom.subplot_grid(Geom.point, Geom.smooth, free_x_axis=true),
     Scale.color_discrete_manual(convert(Vector{Color}, wong_colors())...),
     Guide.ylabel("Pemenuhan Kebutuhan per Biaya (Rp/Rp)"),
