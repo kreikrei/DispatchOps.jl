b1197c1b58b5a63fad952acc839c4792bf8d367d
kreikrei <kreitonstrs@gmail.com>
2022-06-03 22:42:26 +0700

    new images

--- out/forecast.jl
+++ out/forecast.jl
@@ -12,9 +12,17 @@ import Makie: Makie.wong_colors
 exp010 = load_object("/home/kreiton/.julia/dev/DispatchOps/out/exp010.jld2")
 exp011 = load_object("/home/kreiton/.julia/dev/DispatchOps/out/exp011.jld2")
 
-insertcols!(exp010, :H, :noise_function => fill(:static, nrow(exp010)))
-insertcols!(exp011, :H, :noise_function => fill(:dynamic, nrow(exp011)))
+insertcols!(exp010, :H, :noise_function => fill(:Statis, nrow(exp010)))
+insertcols!(exp011, :H, :noise_function => fill(:Dinamis, nrow(exp011)))
 expo = vcat(exp010, exp011)
+expo = expo[expo.noise.!=0, :]
+expo = DataFrame(
+    noise=expo.noise,
+    noise_function=expo.noise_function,
+    H=string.(expo.H),
+    GAP=expo.GAP,
+    simulation=expo.simulation
+)
 
 transform!(
     expo, :simulation => ByRow(x -> total_cost(x) * 1e3) => :total_cost
@@ -24,17 +32,34 @@ transform!(
     expo, :simulation => ByRow(x -> lost_sales(x)) => :lost_sales
 )
 
-p_noise_total_cost = plot(expo,
-    x=:noise, y=:total_cost, xgroup=:noise_function, color=:H,
-    Geom.subplot_grid(Geom.point, Geom.smooth, free_x_axis=true),
-    Scale.y_continuous(labels=x -> "$(x/1e9)"),
-    Scale.color_discrete_manual(convert(Vector{Color}, wong_colors())...),
-    Guide.ylabel("Total Biaya (Miliar Rupiah)"),
-    Guide.xlabel("Parameter Simpangan Tiap Noise Function"),
-    Guide.title("Biaya Total Layanan Terhadap Parameter Simpangan")
-) |> SVG("/home/kreiton/.julia/dev/DispatchOps/out/forecast_fulfillment_per_cost.svg")
+for n in [:Statis, :Dinamis], f in [:total_cost, :lost_sales]
+    p = plot(expo[expo.noise_function.==n, :],
+        x=:noise, y=f, color=:H, Geom.boxplot,
+        Scale.x_discrete(
+            levels=unique(
+                expo[expo.noise_function.==n, :noise]
+            )
+        ),
+        Scale.color_discrete_manual(convert(Vector{Color}, wong_colors())...),
+        Guide.xrug,
+        Guide.yrug,
+        Guide.xlabel("Parameter Simpangan Fungsi $(n)")
+    )
+    f == :total_cost && push!(p,
+        Coord.Cartesian(ymax=4.2e10, ymin=2.5e10),
+        Scale.y_continuous(labels=x -> "$(x/1e9)"),
+        Guide.ylabel("Biaya Total Layanan (Miliar Rupiah)")
+    )
+    f == :lost_sales && push!(p,
+        Coord.Cartesian(ymax=0.27, ymin=0.23),
+        Scale.y_continuous(labels=x -> "$(x*100)%"),
+        Guide.ylabel("Lost Sales")
+    )
 
-p_noise_lost = plot(expo,
+    p |> SVG("/home/kreiton/.julia/dev/DispatchOps/out/forecast_$(f)_$(n).svg")
+end
+
+#=p_noise_lost = plot(expo,
     x=:noise, y=:lost_sales, xgroup=:noise_function, color=:H,
     Geom.subplot_grid(Geom.point, Geom.smooth, free_x_axis=true),
     Scale.y_continuous(labels=x -> "$(x*1e2)%"),
@@ -42,4 +67,4 @@ p_noise_lost = plot(expo,
     Guide.ylabel("Lost Sales"),
     Guide.xlabel("Parameter Simpangan Tiap Noise Function"),
     Guide.title("Lost Sales Terhadap Parameter Simpangan")
-) |> SVG("/home/kreiton/.julia/dev/DispatchOps/out/forecast_lost_sales.svg")
+) |> SVG("/home/kreiton/.julia/dev/DispatchOps/out/forecast_lost_sales.svg")=#
